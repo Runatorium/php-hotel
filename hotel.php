f7b8e13f654031e46f9ca115f5991f0f144f8830
Runatorium <stefano.alesci1@gmail.com>
2023-04-18 16:52:47 +0200

    Esercizio base completato

--- hotel.php
+++ hotel.php
@@ -51,27 +51,32 @@ $parking;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
-    <ul>
+    <div class="list-group">
         <?php
         foreach ($hotels as $hotel) {
-            echo "<li>" . $hotel['name'];
-            echo "<li>" . $hotel['description'];
+            echo "<a class='list-group-item list-group-item-action'></a>";
+            echo "<div class='d-flex w-100 justify-content-between'>";
+            echo "<h5 class='mb-1'>" . $hotel['name'] . "</h5>";
+            echo " </div>";
             if ($hotel['parking']) {
                 $parking = 'disponibile';
             } else {
                 $parking = 'Non disponibile';
             }
-            echo "<li>" . $parking;
-            echo "<li>" . $hotel['vote'];
-            echo "<li>" . $hotel['distance_to_center'];
+            echo "<h5 class='mb-1'>" . $hotel['description'] . "</h5>";
+            echo "<h6 class='mb-1'>" . 'parcheggio : ' . $parking . "</h6>";
+            echo "<small>" . $hotel['vote'] . "</small>";
+            echo "<small>" . $hotel['distance_to_center'] . "</small>";
             echo "<hr>";
         }
         ?>
-    </ul>
+    </div>
 </body>
 
 </html>
